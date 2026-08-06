fbd0e8f94c54a317f9c83db37d8f21d1ea0f0942
fardi <twinaxeenjoyer@gmail.com>
2026-08-06 14:14:31 +0200

    cpp08 finished

---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,31 @@
+#include "MutantStack.hpp"
+#include <iostream>
+
+int main()
+{
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+	std::cout << "Print all numbers test\n";
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+}
+
