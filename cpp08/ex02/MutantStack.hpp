af7acbd7c832d5734964ab5fd71ad487f87d9365
Fardi <twinaxeenjoyer@gmail.com>
2026-08-14 13:44:08 +0200

    fdafad

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -9,6 +9,8 @@ class	MutantStack : public std::stack<T>
 
 		typedef typename std::stack<T>::container_type::iterator		iterator;
 		typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 		MutantStack()
 		{
@@ -45,5 +47,23 @@ class	MutantStack : public std::stack<T>
 		const_iterator	end() const
 		{
 			return (this->c.end());
+		};
+		
+		reverse_iterator rbegin()
+		{
+			return (this->c.rbegin());
+		};
+		reverse_iterator rend()
+		{
+			return (this->c.rend());
+		};
+
+		const_reverse_iterator rbegin() const
+		{
+			return (this->c.rbegin());
+		};
+		const_reverse_iterator rend() const
+		{
+			return (this->c.rend());
 		};
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -21,11 +21,45 @@ int main()
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
 
+	std::cout << "++ and -- test\n";
+	std::cout << *it << " ++test" << std::endl;
+	++it;
+	std::cout << *it << std::endl;
+	std::cout << " --test" << std::endl;
+	--it;
+	std::cout << *it << std::endl;
+	
+	
 	std::cout << "Print all numbers test\n";
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
     }
-}
 
+
+    std::cout << "\n========== RBEGIN / REND TEST ==========" << std::endl;
+
+    MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+    MutantStack<int>::reverse_iterator rite = mstack.rend();
+
+    std::cout << "rbegin(): " << *rit << std::endl;
+
+    std::cout << "Reverse contents:" << std::endl;
+
+    while (rit != rite)
+    {
+        std::cout << *rit << std::endl;
+        ++rit;
+    }
+	
+	
+	std::cout << "\n========== EMPTY STACK ==========" << std::endl;
+
+    MutantStack<int> empty;
+
+    std::cout << "Empty: " << empty.empty() << std::endl;
+    std::cout << "Size: " << empty.size() << std::endl;
+
+
+}
